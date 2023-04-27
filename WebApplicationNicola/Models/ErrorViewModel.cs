da331928df56207a4d02fc6432402d136378c677
Nicola Emmolo <nicola.emmolo@avanade.com>
2023-04-27 10:20:08 +0200

    Add project files.

---
+++ WebApplicationNicola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationNicola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
